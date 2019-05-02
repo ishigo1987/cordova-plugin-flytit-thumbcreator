2985939cd185f890aadb07cc1b5399f94226ab08
ishigo1987 <stephnguets@yahoo.fr>
2019-05-02 16:53:24 +0100

    remove typeof ect...

--- www/ThumbCreator.js
+++ www/ThumbCreator.js
@@ -8,9 +8,6 @@ ThumbCreator.prototype.createThumb = function(fromPath, toPath, successCallback,
     var args = [fromPath, toPath];
     exec(function(result){
 //         console.log("success from native", result);
-            if(typeof(result) !== 'string'){
-              console.log(result)
-            }
             successCallback(result);
         },
         function (result) {
